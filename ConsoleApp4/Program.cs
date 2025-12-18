1c2df196d46041ebde4620c099460175ba3f4ff3
Akash-tech102 <binduramesh0707@gmail.com>
2025-12-18 10:21:50 +0530

    Update Program.cs

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string City { get; set; }
+
+    public static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>
+    {
+        new Person { Name = "Akash", Age = 18, City = "Vatakara"},
+        new Person { Name = "asdfgh", Age = 48, City = "Vatakara"},
+        new Person { Name = "fgh", Age = 58, City = "rtyh"},
+        new Person { Name = "oulk", Age = 28, City = "Vatakara"},
+        new Person { Name = "poiu", Age = 64, City = "qwerty"},
+    };
+
+        var result = people
+                     .Where(p => p.Age > 25)
+                     .Where(p => p.City == "Vatakara")
+                     .Select(p => new
+                     {
+                         p.Name,
+                         p.Age
+
+                     });
+
+        foreach (var person in result)
+        {
+            Console.WriteLine($"{person.Name} - {person.Age}");
+        }
+
+    }
+}
